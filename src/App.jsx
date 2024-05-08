50dbe926f1c342de34c38cee01142a9eb72877da
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-05-08 09:30:54 +0600

    Add Footer And Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
-      <Outlet></Outlet>
+      <div className="min-h-[calc(100vh-306px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </>
   );
 }
--- src/Components/Router.jsx
+++ src/Components/Router.jsx
@@ -2,12 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../Components/Pages/Login";
 import Register from "../Components/Pages/Register";
+import Home from "../Components/Pages/Home";
 
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
     children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
       {
         path: "/login",
         element: <Login></Login>,
