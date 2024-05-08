bcd1a3787d6b4222f6267d7578018a199af0eea5
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-05-08 09:21:50 +0600

    Complete Login And Register Page

--- src/Components/Pages/Register.jsx
+++ src/Components/Pages/Register.jsx
@@ -1,17 +1,10 @@
 import { Link } from "react-router-dom";
-import login from "../../assets/images/login.jpg";
+import register from "../../assets/images/register.jpg";
 
-const Login = () => {
+const Registration = () => {
   return (
-    <div className="flex justify-center items-center min-h-[calc(100vh-306px)] py-8">
+    <div className="flex justify-center items-center min-h-[calc(100vh-306px)] py-10">
       <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg  lg:max-w-4xl ">
-        <div
-          className="hidden bg-cover bg-center lg:block lg:w-1/2"
-          style={{
-            backgroundImage: `url(${login})`,
-          }}
-        ></div>
-
         <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
           <div className="flex justify-center mx-auto">
             <img
@@ -22,7 +15,7 @@ const Login = () => {
           </div>
 
           <p className="mt-3 text-xl text-center text-gray-600 ">
-            Welcome back!
+            Get Your Free Account Now.
           </p>
 
           <div className="flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 ">
@@ -56,12 +49,42 @@ const Login = () => {
             <span className="w-1/5 border-b  lg:w-1/4"></span>
 
             <div className="text-xs text-center text-gray-500 uppercase  hover:underline">
-              or login with email
+              or Registration with email
             </div>
 
             <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
           </div>
           <form>
+            <div className="mt-4">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-600 "
+                htmlFor="name"
+              >
+                Username
+              </label>
+              <input
+                id="name"
+                autoComplete="name"
+                name="name"
+                className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
+                type="text"
+              />
+            </div>
+            <div className="mt-4">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-600 "
+                htmlFor="photo"
+              >
+                Photo URL
+              </label>
+              <input
+                id="photo"
+                autoComplete="photo"
+                name="photo"
+                className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
+                type="text"
+              />
+            </div>
             <div className="mt-4">
               <label
                 className="block mb-2 text-sm font-medium text-gray-600 "
@@ -101,7 +124,7 @@ const Login = () => {
                 type="submit"
                 className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
               >
-                Sign In
+                Sign Up
               </button>
             </div>
           </form>
@@ -110,18 +133,24 @@ const Login = () => {
             <span className="w-1/5 border-b  md:w-1/4"></span>
 
             <Link
-              to="/register"
+              to="/login"
               className="text-xs text-gray-500 uppercase  hover:underline"
             >
-              or sign up
+              or sign in
             </Link>
 
             <span className="w-1/5 border-b  md:w-1/4"></span>
           </div>
         </div>
+        <div
+          className="hidden bg-cover bg-center lg:block lg:w-1/2"
+          style={{
+            backgroundImage: `url(${register})`,
+          }}
+        ></div>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Registration;
--- src/Components/Router.jsx
+++ src/Components/Router.jsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Login from "../Components/Pages/Login";
+import Register from "../Components/Pages/Register";
 
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+    children: [
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
 export default Router;
