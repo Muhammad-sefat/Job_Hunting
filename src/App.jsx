ddca16f559c308bd366144774a73a6960e6e3d6a
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-05-08 08:59:38 +0600

    Add AuthProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 
-// https://l.facebook.com/l.php?u=https%3A%2F%2Fgithub.com%2Fshakilahmedatik%2FsoloSphere-resources%3Ffbclid%3DIwZXh0bgNhZW0CMTAAAR3Jmh8uxRgVzdUEcphgaDS5GHY8E6lwD5PwH2CFxhGTYBYGA3wYKl5Y1nI_aem_Ae6UQsyF2Dq46uFsOOxMAUV3eQu96jprcbqUrnIi2REdcBfm_7_4NGmiKrsUjcm_AzLsIIBAb9ENsQNIJhSeEal_&h=AT1LwkPEu41AQQgCGnyR5Ke-JJ6wFjulyVuTVxGBVC_wcS4FHXEXfixW5AwtCid58zB54rUzuOoWrXzJrgM0PDJ7qmKlDQ5y5ZuUbeNiw4e4zjbAPTnzgGbrnEOFwaa3vBi1&__tn__=R]-R&c[0]=AT3NMwPhdcSSx3W-jKctIsIi6l9cKBSttGamKhlHDrVrNH6sYI3haB6mgMDWrs71WOcV4ZDrrz7IZm-E9hrulUydFNRzMSutWEzIyYFHtlFrQoV1RW_Tm_6OuEjhKZKhhFuxRstOJaUh1tEr6bMnDIvqaT3SPlTB5MQ-c4RU031sybVAKDiA7wdvUm3mxCnS0uTK1ex697Zx05-gh4y7mWFNeWw7fhgfhuByZgCo
-
 function App() {
   return (
     <>
---
+++ src/Components/Firebase.config.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC8PFNtB-WaVucOFS9zZ7V4vlrr9bZ13oc",
+  authDomain: "job-hunting-cdb17.firebaseapp.com",
+  projectId: "job-hunting-cdb17",
+  storageBucket: "job-hunting-cdb17.appspot.com",
+  messagingSenderId: "256046224401",
+  appId: "1:256046224401:web:dee978dc76c2bcfec84db0",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
